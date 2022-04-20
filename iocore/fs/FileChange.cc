b4c02709886e3436e65cbf4e6fe28b023cb4b2d6
Mo Chen <uncorrupt@gmail.com>
2022-04-20 20:41:48 +0000

    Add watch delete code

--- iocore/fs/FileChange.cc
+++ iocore/fs/FileChange.cc
@@ -23,6 +23,7 @@
 
 #include "FileChange.h"
 #include "tscore/Diags.h"
+#include "P_EventSystem.h"
 
 #include <cassert>
 #include <functional>
@@ -35,7 +36,60 @@ static constexpr size_t INOTIFY_BUF_SIZE = 4096;
 #if TS_USE_INOTIFY
 
 static void
-inotify_thread(int inotify_fd, std::map<int, struct file_info> &file_watches, std::map<int, struct dir_info> &dir_watches)
+process_file_event(struct inotify_event *event, int inotify_fd, std::unordered_map<int, struct file_info> &file_watches)
+{
+  auto finfo_it = file_watches.find(event->wd);
+  if (finfo_it != file_watches.end()) {
+    const struct file_info &finfo = finfo_it->second;
+    if (event->mask & (IN_CLOSE_WRITE | IN_ATTRIB)) {
+      Debug(TAG, "Modify file event (%d) on %s", event->mask, finfo.path.c_str());
+      eventProcessor.schedule_imm(finfo.contp, ET_TASK);
+    } else if (event->mask & (IN_DELETE_SELF | IN_MOVED_FROM)) {
+      Debug(TAG, "Delete file event (%d) on %s", event->mask, finfo.path.c_str());
+      int rc2 = inotify_rm_watch(inotify_fd, event->wd);
+      if (rc2 == -1) {
+        Error("Failed to remove inotify watch on %s: %s (%d)", finfo.path.c_str(), strerror(errno), errno);
+      }
+      eventProcessor.schedule_imm(finfo.contp, ET_TASK);
+      file_watches.erase(event->wd);
+    }
+  }
+}
+
+static void
+process_dir_event(struct inotify_event *event, int inotify_fd, std::unordered_map<int, struct file_info> &file_watches,
+                  std::unordered_map<int, struct dir_info> &dir_watches,
+                  std::unordered_map<int, struct watch_handle_info> watch_handles)
+{
+  auto dinfo_it = dir_watches.find(event->wd);
+  if (dinfo_it != dir_watches.end()) {
+    const struct dir_info &dinfo = dinfo_it->second;
+    if (event->mask & (IN_CREATE | IN_MOVED_TO | IN_ATTRIB)) {
+      Debug(TAG, "Create file event (%d) on %s", event->mask, dinfo.dname.c_str());
+      auto file_it = dinfo.files.find(event->name);
+      if (file_it != dinfo.files.end()) {
+        // This is a file we care about
+        std::filesystem::path full_path{dinfo.dname};
+        full_path /= event->name;
+        for (auto &[watch_handle, finfo] : file_it->second) {
+          int wd = inotify_add_watch(inotify_fd, full_path.c_str(), IN_DELETE_SELF | IN_CLOSE_WRITE);
+          if (wd == -1) {
+            Error("Failed to add inotify watch on %s: %s (%d)", full_path.c_str(), strerror(errno), errno);
+          } else {
+            watch_handles[watch_handle].file_wd = wd;
+            file_watches[wd]                    = finfo;
+          }
+          eventProcessor.schedule_imm(finfo.contp, ET_TASK);
+        }
+      }
+    }
+  }
+}
+
+static void
+inotify_thread(int inotify_fd, std::unordered_map<int, struct file_info> &file_watches,
+               std::unordered_map<int, struct dir_info> &dir_watches,
+               std::unordered_map<int, struct watch_handle_info> watch_handles)
 {
   for (;;) {
     char inotify_buf[INOTIFY_BUF_SIZE];
@@ -56,40 +110,10 @@ inotify_thread(int inotify_fd, std::map<int, struct file_info> &file_watches, st
       struct inotify_event *event = reinterpret_cast<struct inotify_event *>(inotify_buf);
 
       // Process file events
-      auto finfo_it = file_watches.find(event->wd);
-      if (finfo_it != file_watches.end()) {
-        const struct file_info &finfo = finfo_it->second;
-        if (event->mask & (IN_CLOSE_WRITE | IN_ATTRIB)) {
-          Debug(TAG, "Modify file event (%d) on %s", event->mask, finfo.path.c_str());
-          // TODO: call contp
-        } else if (event->mask & (IN_DELETE_SELF | IN_MOVED_FROM)) {
-          Debug(TAG, "Delete file event (%d) on %s", event->mask, finfo.path.c_str());
-          int rc2 = inotify_rm_watch(inotify_fd, event->wd);
-          if (rc2 == -1) {
-            Error("Failed to remove inotify watch on %s: %s (%d)", finfo.path.c_str(), strerror(errno), errno);
-          }
-          file_watches.erase(event->wd);
-          // TODO: call contp
-        }
-      }
+      process_file_event(event, inotify_fd, file_watches);
 
       // Process directory events
-      auto dinfo_it = dir_watches.find(event->wd);
-      if (dinfo_it != dir_watches.end()) {
-        const struct dir_info &dinfo = dinfo_it->second;
-        if (event->mask & (IN_CREATE | IN_MOVED_TO)) {
-          Debug(TAG, "Create file event (%d) on %s", event->mask, dinfo.dname.c_str());
-          auto file_it = dinfo.files.find(event->name);
-          if (file_it != dinfo.files.end()) {
-            // This is a file we care about
-            std::filesystem::path full_path{dinfo.dname};
-            full_path /= event->name;
-            int wd           = inotify_add_watch(inotify_fd, full_path.c_str(), IN_DELETE_SELF | IN_CLOSE_WRITE | IN_ATTRIB);
-            file_watches[wd] = {full_path, file_it->second};
-          }
-          // TODO: call contp
-        }
-      }
+      process_dir_event(event, inotify_fd, file_watches, dir_watches, watch_handles);
     }
   }
 }
@@ -108,7 +132,7 @@ FileChangeManager::init()
     return;
   }
 
-  poll_thread = std::thread(inotify_thread, inotify_fd, std::ref(file_watches), std::ref(dir_watches));
+  poll_thread = std::thread(inotify_thread, inotify_fd, std::ref(file_watches), std::ref(dir_watches), std::ref(watch_handles));
   poll_thread.detach();
 #else
   // Implement this
@@ -120,29 +144,98 @@ FileChangeManager::init()
 int
 FileChangeManager::add_directory_watch(const std::filesystem::path &file_path, Continuation *contp)
 {
+#if TS_USE_INOTIFY
   auto dname = file_path.parent_path();
-  auto wd    = inotify_add_watch(inotify_fd, dname.c_str(), IN_CREATE | IN_MOVED_TO);
-  return wd;
+  for (const auto &[wd, dinfo] : dir_watches) {
+    if (dinfo.dname == dname) {
+      return wd;
+    }
+  }
+  auto new_wd = inotify_add_watch(inotify_fd, dname.c_str(), IN_CREATE | IN_MOVED_TO);
+  if (new_wd == -1) {
+    Error("Failed to add directory watch to %s: %s, (%d)", file_path.c_str(), strerror(errno), errno);
+  }
+
+  dir_watches[new_wd] = {dname, {{file_path.filename(), {{new_wd, {file_path, contp}}}}}};
+  return new_wd;
+#else
+  // Implement this
+#endif
 }
 
 int
 FileChangeManager::add(const std::filesystem::path &path, Continuation *contp)
 {
+  Debug(TAG, "Adding a watch on %s", path.c_str());
+  if (free_handles.empty()) {
+    Error("Maximum number of file watches reached.");
+    return -1;
+  }
+
 #if TS_USE_INOTIFY
+  auto dir_wd = add_directory_watch(path, contp);
+  if (dir_wd == -1) {
+    return -1;
+  }
   // Let the OS handle multiple watches on one file.
   auto wd = inotify_add_watch(inotify_fd, path.c_str(), IN_DELETE_SELF | IN_CLOSE_WRITE | IN_ATTRIB);
   if (wd == -1) {
     Debug(TAG, "add(%s) resulted in %s (%d)", path.c_str(), strerror(errno), errno);
     if (errno == EACCES || errno == ENOENT) {
-      return add_directory_watch(path, contp);
+      // This is fine.  We added a watch to the parent directory in case this file gets created later.
     } else {
-      return wd;
+      Error("Failed to add file watch on %s: %s (%d)", path.c_str(), strerror(errno), errno);
+      inotify_rm_watch(inotify_fd, dir_wd);
+      return -1;
     }
   } else {
     file_watches[wd] = {path, contp};
-    return wd;
   }
+
+  auto new_handle = free_handles.back();
+  free_handles.pop_back();
+  watch_handles[new_handle] = {.file_wd = wd, .dir_wd = dir_wd};
+  return new_handle;
 #else
   // Implement this
 #endif
+}
+
+void
+FileChangeManager::remove(int watch_handle)
+{
+  Debug(TAG, "Removing watch handle %d", watch_handle);
+  if (watch_handles.find(watch_handle) == watch_handles.end()) {
+    Debug(TAG, "Tried to remove non-existant watch handle: %d", watch_handle);
+    return;
+  }
+
+  const auto &whinfo = watch_handles[watch_handle];
+
+  assert(file_watches.find(whinfo.file_wd) != file_watches.end());
+  if (whinfo.file_wd != -1) {
+    if (inotify_rm_watch(inotify_fd, whinfo.file_wd) == -1) {
+      Error("Failed to remove watch on %s: %s (%d)", file_watches[whinfo.file_wd].path.c_str(), strerror(errno), errno);
+    }
+  }
+  file_watches.erase(whinfo.file_wd);
+
+  assert(dir_watches.find(whinfo.dir_wd) != dir_watches.end());
+  auto &dir_info = dir_watches[whinfo.dir_wd];
+  // Navigate the watches under this directory to find the correct one
+  for (auto &[filename, finfos] : dir_info.files) {
+    auto kv = finfos.find(watch_handle);
+    if (kv != finfos.end()) {
+      if (finfos.size() == 1) {
+        // We are the last one to remove this dir watch
+        if (inotify_rm_watch(inotify_fd, whinfo.dir_wd) == -1) {
+          Error("Failed to remove directory watch on %s: %s (%d)", dir_info.dname.c_str(), strerror(errno), errno);
+        }
+        dir_watches.erase(whinfo.dir_wd);
+      } else {
+        assert(finfos.size() > 1);
+        finfos.erase(watch_handle);
+      }
+    }
+  }
 }
--- iocore/fs/FileChange.h
+++ iocore/fs/FileChange.h
@@ -28,8 +28,9 @@
 #include <chrono>
 #include <filesystem>
 #include <set>
-#include <map>
+#include <unordered_map>
 #include <vector>
+#include <shared_mutex>
 #include "P_EventSystem.h"
 
 // TODO: detect this with autotools
@@ -41,31 +42,79 @@
 // implement this
 #endif
 
+using watch_handle_t = int;
+
 // File watch info
 struct file_info {
   std::filesystem::path path;
   Continuation *contp;
 };
 
+// A map of watched files under a directory:
+//
+//  filename:
+//    watch_handle:
+//      path
+//      continuation
+//    watch_handle:
+//      path
+//      conttinuation
+//
+// 1. A file can have multiple watch handles.
+// 2. Different watch handles can have the same or different continuations.
+// 3. Multiple files can be watched under one directory.
+using DirectoryWatchMap = std::unordered_map<std::string, std::unordered_map<watch_handle_t, struct file_info>>;
+
 // Directory watch info, for files that don't exist yet
 struct dir_info {
   std::filesystem::path dname;
-  std::map<const std::string, Continuation *> files;
+  DirectoryWatchMap files; // All of the watched files in this directory
 };
+
+struct watch_handle_info {
+  int file_wd; // can be -1 or a real wd
+  int dir_wd;  // can only be a real wd
+};
+
+constexpr int MAX_WATCHES = 10000;
 
 class FileChangeManager
 {
 public:
+  FileChangeManager()
+  {
+    for (int i = 0; i < MAX_WATCHES; i++) {
+      free_handles.push_back(i);
+    }
+  }
+
   void init();
+
+  /**
+    Add a file watch
+
+    @return a watch handle, or -1 on error
+  */
   int add(const std::filesystem::path &path, Continuation *contp);
+
+  /**
+    Remove a file watch
+  */
+  void remove(int watch_handle);
 
 private:
   std::thread poll_thread;
 
   int add_directory_watch(const std::filesystem::path &file_path, Continuation *contp);
+
+  std::shared_mutex watch_handle_mutex;
+  std::unordered_map<int, struct watch_handle_info> watch_handles;
+  std::vector<int> free_handles;
 #if TS_USE_INOTIFY
-  std::map<int, struct file_info> file_watches;
-  std::map<int, struct dir_info> dir_watches;
+  std::shared_mutex file_watches_mutex;
+  std::unordered_map<int, struct file_info> file_watches;
+  std::shared_mutex dir_watches_mutex;
+  std::unordered_map<int, struct dir_info> dir_watches;
   int inotify_fd;
 #else
   // implement this
