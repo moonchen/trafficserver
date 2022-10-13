2c3c3b0edbce441241a737b7a1d0005eff76c25d
Mo Chen <uncorrupt@gmail.com>
2022-10-12 23:57:27 -0500

    Network IO-Uring: initial
    Spawn 4 io_uring threads that do nothing for now.

---
+++ iocore/net/IOUringNet.cc
@@ -0,0 +1,82 @@
+/** @file IOUringNet
+
+  Implementation of io_uring net handler.
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+#include "P_IOUringNet.h"
+#include "tscore/ink_assert.h"
+#include <liburing.h>
+
+constexpr auto TAG = "io_uring";
+
+thread_local IOUringNetHandler *inh = nullptr;
+
+void
+IOUringNetHandler::signalActivity()
+{
+}
+
+int
+IOUringNetHandler::waitForActivity(ink_hrtime timeout)
+{
+  struct io_uring_cqe *cqe;
+
+  if (pending <= 0) {
+    Debug(TAG, "sleeping");
+    sleep(1);
+  }
+
+  while (pending > 0) {
+    // No timeout because we're not stealing work from other threads
+    Debug(TAG, "waiting for cqe");
+    auto ret = io_uring_wait_cqe(&ring, &cqe);
+    if (ret < 0) {
+      Warning("io_uring_wait_cqe failed: %s", strerror(-ret));
+    } else {
+      pending--;
+      // TODO: handle completed I/O
+      // TODO: break here if new events are scheduled by I/O completion handlers
+    }
+  }
+
+  return 0;
+}
+
+void
+initialize_thread_for_iouring(EThread *thread)
+{
+  ink_release_assert(inh == nullptr);
+  inh = new IOUringNetHandler();
+  ink_release_assert(inh != nullptr);
+
+  // TODO: replace placeholder values
+  const int queue_depth = 1024;
+  io_uring_params p{};
+
+  Debug(TAG, "Started io_uring thread");
+
+  auto ret = io_uring_queue_init_params(queue_depth, &inh->ring, &p);
+  if (ret < 0) {
+    Fatal("Failed to initialize io_uring: %s", strerror(-ret));
+    return;
+  }
+
+  thread->set_tail_handler(inh);
+}
---
+++ iocore/net/P_IOUringNetVConnection.h
@@ -0,0 +1,58 @@
+/** @file
+
+  A brief file description
+
+  @section license License
+
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+#pragma once
+
+#include "I_NetVConnection.h"
+
+class IOUringNetVConnection : public NetVConnection
+{
+public:
+  IOUringNetVConnection();
+  ~IOUringNetVConnection() override;
+
+  VIO *do_io_read(Continuation *c, int64_t nbytes, MIOBuffer *buf) override;
+  VIO *do_io_write(Continuation *c, int64_t nbytes, IOBufferReader *buf, bool owner = false) override;
+  void do_io_close(int lerrno = -1) override;
+  void do_io_shutdown(ShutdownHowTo_t howto) override;
+  void set_active_timeout(ink_hrtime timeout_in) override;
+  void set_inactivity_timeout(ink_hrtime timeout_in) override;
+  void set_default_inactivity_timeout(ink_hrtime timeout_in) override;
+  bool is_default_inactivity_timeout() override;
+  void cancel_active_timeout() override;
+  void cancel_inactivity_timeout() override;
+  void add_to_keep_alive_queue() override;
+  void remove_from_keep_alive_queue() override;
+  bool add_to_active_queue() override;
+  ink_hrtime get_active_timeout() override;
+  ink_hrtime get_inactivity_timeout() override;
+  void apply_options() override;
+  void reenable(VIO *vio) override;
+  void reenable_re(VIO *vio) override;
+  SOCKET get_socket() override;
+  int set_tcp_congestion_control(int side) override;
+  void set_local_addr() override;
+  void set_remote_addr() override;
+  void set_remote_addr(const sockaddr *) override;
+  void set_mptcp_state() override;
+};
